6e1edcc4c386eef8a0ac082f999cf401a4c8d2af
Aurélien Dufour <aureluendufour7@gmail.com>
2024-12-04 15:22:32 +0100

    chore: change angular version to 18 and add primeng

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DockModule} from 'primeng/dock';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    DockModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Chrono button',
+        icon: 'assets/img/Chrono.png'
+      },
+      {
+        label: 'Swords button',
+        icon: 'assets/img/Swords.png'
+      },
+      {
+        label: 'Shuffle button',
+        icon: 'assets/img/Shuffle.png'
+      }
+    ];
+  }
 
 }
